fe3be5a0ff2c415521db9acb93f47b52c4d9447e
khaled39ab <khaled39ab@gmail.com>
2022-06-14 12:41:59 +0600

    header complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../../logo.svg'
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav>
+            <div className='logo-name'>
+                <img src={logo} alt="" />
+                <h2>Router Firebase Integration</h2>
+            </div>
+            <div className='custom-link'>
+                <CustomLink to={'/'}>HOME</CustomLink>
+                <CustomLink to={'/reviews'}>REVIEWS</CustomLink>
+                <CustomLink to={'/dashboard'}>DASHBOARD</CustomLink>
+                <CustomLink to={'/blogs'}>BLOGS</CustomLink>
+                <CustomLink to={'/about'}>ABOUT</CustomLink>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
