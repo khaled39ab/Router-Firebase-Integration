ec9d410eb9a2d35f5bd59af3798818a5fa9c044f
khaled39ab <khaled39ab@gmail.com>
2022-06-15 23:18:13 +0600

    create google & github sign in btn

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import LogIn from './components/LogIn/LogIn';
-import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/logIn' element={<LogIn></LogIn>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
     </div>
   );
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,44 +1,21 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './LogIn.css'
-import { Link } from 'react-router-dom';
 
 const LogIn = () => {
-    const [validated, setValidated] = useState(false);
 
     return (
         <div className='logIn-container p-4 m-4 w-50 mx-auto'>
             <h2 className='text-center text-info'>Please Log In</h2>
-            <Form noValidate validated={validated}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid email.
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid password.
-                    </Form.Control.Feedback>
-                </Form.Group>
-
+            <div className='mx-auto btn-sec'>
                 <Button variant="primary" type="submit">
-                    Log in
+                    Log in with Google
                 </Button>
-                <Button variant="link">Forget Password?</Button>
-                <hr />
-            </Form>
-            <div className='create-btn-sec'>
-                <Link to={'/register'}>
-                    <Button>
-                        Create An Account
-                    </Button>
-                </Link>
+                <br />
+                <Button variant="primary" type="submit">
+                    Log in with Github
+                </Button>
             </div>
         </div>
     );
