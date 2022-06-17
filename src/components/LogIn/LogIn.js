f2f3760495dbfb07d8ae8e4ab386a451e026c5ef
khaled39ab <khaled39ab@gmail.com>
2022-06-18 00:29:58 +0600

    redirected to the login page using  replace: true

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -6,17 +6,28 @@ import { getAuth } from 'firebase/auth';
 import app from '../../firebase.init';
 // import useFirebase from '../../hooks/useFirebase';
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const auth = getAuth(app)
 const LogIn = () => {
     // const { signInWithGoogle } = useFirebase();
-    const [signInWithGoogle] = useSignInWithGoogle(auth)
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
 
+    const from = location.state.from.pathname || '/';
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+        .then(() =>{
+            navigate (from, {replace: true});
+        })
+    }
     return (
         <div className='logIn-container p-4 m-4 w-50 mx-auto'>
             <h2 className='text-center text-info'>Please Log In</h2>
             <div className='mx-auto btn-sec'>
-                <Button onClick={() => signInWithGoogle()} variant="primary" type="submit">
+                <Button onClick={handleGoogleSignIn} variant="primary" type="submit">
                     Log in with Google
                 </Button>
                 <br />
