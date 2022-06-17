b3eb7429d74b06c306527f6565a0fc99eff6becd
khaled39ab <khaled39ab@gmail.com>
2022-06-18 00:10:42 +0600

    Protected Route and Require Auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import LogIn from './components/LogIn/LogIn';
+import RequireAuth from './components/RequireAuth/RequireAuth';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
@@ -11,6 +13,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={
+          <RequireAuth>
+            <Dashboard></Dashboard>
+          </RequireAuth>
+        }></Route>
         <Route path='/logIn' element={<LogIn></LogIn>}></Route>
       </Routes>
     </div>
