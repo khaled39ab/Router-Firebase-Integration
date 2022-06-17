8b9b8ad2aebb1d5e64569f4e507d50f72786eaa2
khaled39ab <khaled39ab@gmail.com>
2022-06-17 23:08:40 +0600

    displaying user name on navbar with react-firebase-hooks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import app from '../../firebase.init';
+// import useFirebase from '../../hooks/useFirebase';
 import logo from '../../logo.svg'
 import CustomLink from '../CustomLink/CustomLink';
 import './Header.css'
 
+const auth = getAuth(app)
+
 const Header = () => {
-    const {user, handleSignOut} = useFirebase()
+    // const {user, handleSignOut} = useFirebase()
+    const [user] = useAuthState(auth)
     return (
         <nav>
             <div className='logo-name'>
@@ -20,7 +26,7 @@ const Header = () => {
                 <h5 style={{color:'white'}}>{user?.displayName && user.displayName}</h5>
                 {
                     user?.uid ? 
-                    <button onClick={handleSignOut}>Sign Out</button> :
+                    <button onClick={() => signOut(auth)}>Sign Out</button> :
                     <CustomLink to={'/login'}>LOG IN</CustomLink>
                 }
             </div>
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -2,16 +2,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import './LogIn.css'
-import useFirebase from '../../hooks/useFirebase';
+import { getAuth } from 'firebase/auth';
+import app from '../../firebase.init';
+// import useFirebase from '../../hooks/useFirebase';
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
 
+const auth = getAuth(app)
 const LogIn = () => {
-    const { signInWithGoogle } = useFirebase();
+    // const { signInWithGoogle } = useFirebase();
+    const [signInWithGoogle] = useSignInWithGoogle(auth)
 
     return (
         <div className='logIn-container p-4 m-4 w-50 mx-auto'>
             <h2 className='text-center text-info'>Please Log In</h2>
             <div className='mx-auto btn-sec'>
-                <Button onClick={signInWithGoogle} variant="primary" type="submit">
+                <Button onClick={() => signInWithGoogle()} variant="primary" type="submit">
                     Log in with Google
                 </Button>
                 <br />
